749752b6c520161e656a000cddf4909b14571999
Conor H <conor.heffron@gmail.com>
2024-07-29 06:06:24 +0100

    Add test code & resources

---
+++ src/test/java/com/graphql/chooseapi/grpc/GrpcIntegrationTestConfig.java
@@ -0,0 +1,28 @@
+package com.graphql.chooseapi.grpc;
+
+import com.graphql.chooseapi.services.BooksService;
+import net.devh.boot.grpc.client.autoconfigure.GrpcClientAutoConfiguration;
+import net.devh.boot.grpc.server.autoconfigure.GrpcServerAutoConfiguration;
+import net.devh.boot.grpc.server.autoconfigure.GrpcServerFactoryAutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ImportAutoConfiguration({
+  GrpcServerAutoConfiguration.class, // Create required server beans
+  GrpcServerFactoryAutoConfiguration.class, // Select server implementation
+  GrpcClientAutoConfiguration.class}) // Support @GrpcClient annotation
+class GrpcIntegrationTestConfig {
+
+    @Bean
+    BooksService booksService() {
+        return new BooksService();
+    }
+
+    @Bean
+    BooksServiceGrpc booksServiceGrpc() {
+        return new BooksServiceGrpc(booksService());
+    }
+
+}
